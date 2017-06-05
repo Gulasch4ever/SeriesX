9bc5f9be00b49cf76d3fff903e45f6a67de11245
Gulasch4ever <laszlo.balo@edu.fh-joanneum.at>
2017-06-05 16:37:45 +0200

    update UsersModel

--- src/at/fh/seriesX/model/UsersModel.java
+++ src/at/fh/seriesX/model/UsersModel.java
@@ -1,6 +1,10 @@
 package at.fh.seriesX.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
