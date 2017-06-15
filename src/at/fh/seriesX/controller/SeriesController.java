3e0ffa5378be85be77107d4d088a7a035e42650d
jashanic14 <jashanica@live.at>
2017-06-15 22:54:20 +0200

    added "showAddEmployeeForm" method

--- src/at/fh/seriesX/controller/SeriesController.java
+++ src/at/fh/seriesX/controller/SeriesController.java
@@ -34,4 +34,10 @@ public class SeriesController {
 		return "listSeries";
 	}
 	
+	@RequestMapping(value = { "editSeries" })
+	public String showAddSeriesForm(Model model) {
+		
+		return "editSeries";
+	}
+	
 }
